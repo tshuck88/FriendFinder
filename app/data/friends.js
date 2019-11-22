8b5aca7dcb3b0b24909834e01dd6b8c3d712243c
Tyler Shuck <tshuck88@gmail.com>
2019-11-22 11:00:34 -0800

    created array of friends

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+const friends = [
+    {
+        name: "Bob",
+        photo: "https://755567qrmy-flywheel.netdna-ssl.com/wp-content/uploads/2019/03/Country-Bob.jpg",
+        scores: [1, 2, 3, 4, 5, 4, 3, 2, 1, 2]
+    },
+    {
+        name: "Fred",
+        photo: "https://cdn-02.independent.ie/sport/soccer/article36980072.ece/974cb/AUTOCROP/w620/ipanews_812c63a3-fbd8-4f65-b5dc-75da7b6d83a9_1",
+        scores: [5, 4, 3, 2, 1, 2, 3, 4, 5, 4]
+    },
+    {
+        name: "Barry",
+        photo: "https://pmctvline2.files.wordpress.com/2019/02/barry-season-2-hbo-bill-hader.jpg?w=620&h=440&crop=1",
+        scores: [3, 4, 2, 2, 1, 1, 5, 5, 5, 4]
+    },
+    {
+        name: "Frank",
+        photo: "https://www.wunc.org/sites/wunc/files/styles/x_large/public/201804/Stasio_Frank_2018.jpg",
+        scores: [5, 4, 4, 5, 1, 4, 3, 3, 1, 1]
+    },
+    {
+        name: "Larry",
+        photo: "https://www.metzlewis.com/wp-content/uploads/2016/01/metzlewis-attorney-profile-larry-blair.jpg",
+        scores: [4, 5, 4, 1, 2, 4, 3, 4, 4, 3]
+    },
+    {
+        name: "Sue",
+        photo: "https://m.media-amazon.com/images/M/MV5BMTY1NDc2MDYyNF5BMl5BanBnXkFtZTYwMjcwNDI2._V1_UY317_CR27,0,214,317_AL_.jpg",
+        scores: [4, 3, 2, 1, 3, 4, 5, 4, 3, 2]
+    },
+    {
+        name: "Mary",
+        photo: "https://tiffanyholtz.com/ClientImage/Agent-Full/mwinkel.jpg",
+        scores: [2, 3, 3, 4, 4, 5, 5, 1, 1, 1]
+    }
+]
